bdbcfbbab6ca17c13603a58f4fa64d9b43bb232f
Ryan Du <ryandu343@gmail.com>
2025-06-11 11:14:53 -0400

    feat: enhance extract_pbp_data by adding shot data, flattening qualifiers, and cleaning data

--- extract_game_data.py
+++ extract_game_data.py
@@ -5,12 +5,12 @@ import janitor
 
 def extract_pbp_data(json):
     """
-    Extracts the CEBL play by play data from CEBL JSON.
+    Extracts the CEBL play by play data from JSON response.
 
     Parameters
     ----------
     json : dict
-        The JSON response containing play-by-play data
+        The JSON response containing play-by-play and shot data.
 
     Returns
     -------
@@ -26,20 +26,31 @@ def extract_pbp_data(json):
     pbp_df = pbp_df.rename(columns={'player' : 'scoreboard_name'})
     pbp_df['player'] = pbp_df['first_name'] + ' ' + pbp_df['family_name']
 
+    qualifier_df = pd.DataFrame(pbp_df['qualifier'].tolist()).add_prefix('qualifier_')
+    pbp_df = pd.concat([pbp_df.drop(columns=['qualifier']), qualifier_df], axis=1)
+
+    shot_data_one = json['tm']['1']['shot']
+    shot_data_two = json['tm']['2']['shot']
+    shot_df_one = pd.json_normalize(shot_data_one)
+    shot_df_two = pd.json_normalize(shot_data_two)
+    shot_df = pd.concat([shot_df_one, shot_df_two], ignore_index=True)
+
+    shot_df = shot_df.clean_names(case_type="snake")
+    pbp_df = pbp_df.merge(
+        shot_df[["action_number", "x", "y"]],
+        on="action_number",
+        how="left"
+    )
+
+    pbp_df = pbp_df.fillna(pd.NA)
+    pbp_df = pbp_df.replace({None: pd.NA})
     pbp_df = pbp_df.rename(columns={
-        "gt": "clock",
+        "gt": "game_time",
         "s1": "home_score",
         "s2": "away_score",
         "lead": "home_lead",
         "tno": "team_id",
-        "pno": "player_number",
+        "pno": "player_id",
     })
 
-    column_order = ['period', 'period_type', 'clock', 'home_score', 'away_score', 'home_lead', 'team_id',
-                    'player_number', 'player', 'scoreboard_name', 'action_type', 'sub_type', 'success', 
-                    'scoring', 'qualifier', 'first_name', 'family_name', 'first_name_initial', 'family_name_initial',
-                    'international_first_name_initial', 'international_family_name_initial']
-    pbp_df = pbp_df[column_order]
-    
     return pbp_df
-
